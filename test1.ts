51cdde5625a2d24aee028f0dabccf486f0133955
Peter Šulek <peter.sulek@synotgames.com>
2025-12-14 16:26:15 +0100

    cleanup testing stuff

--- test1.ts
+++
@@ -1,7 +0,0 @@
-import getNpmTarballUrl from 'get-npm-tarball-url'
-
-const url = getNpmTarballUrl('@psulek/lhq-generators', '1.0.0-rc.1', {
-    registry: 'https://npm.pkg.github.com'
-});
-
-console.log(url);
